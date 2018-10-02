22c44838993fb7331aa6cad1efadeab6a078ecc0
Rafay Hasan <rafayhasan@rafays-macbook-pro.local>
2018-10-02 20:29:33 +0600

    Initial Commit

---
+++ Tapstar Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tapstar Demo
+//
+//  Created by Rafay Hasan on 2/10/18.
+//  Copyright © 2018 Rafay Hasan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
